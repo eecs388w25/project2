987705307fb434c7357e8c16e9190ea75eadfee4
rsta <rsta@umich.edu>
2025-01-01 10:58:12 -0800

    Initial release for Winter 2025

---
+++ sql_5.py
@@ -0,0 +1,15 @@
+import sys
+import hashlib
+
+if len(sys.argv) != 3:
+	print("Usage: python3 sql_5.py <uniqname> <password>")
+	exit
+
+uniqname = sys.argv[1]
+password = sys.argv[2]
+
+query = ""
+
+# TODO: Set query to a value as specified in the spec. 
+
+print(query, end="")
